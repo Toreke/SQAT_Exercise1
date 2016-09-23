1af543c09da9c6c096ec497c3caca33f53ceb5a0
somename <someemail>
2016-09-23 15:41:11 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -24,16 +24,36 @@ public class TestBowling {
 	public void test3() {
 		
 		BowlingGame Bgame = new BowlingGame();
+		
 		Frame frame = new Frame(1,5);
-		Frame frame = new Frame(3,6);
-		Frame frame = new Frame(7,2);
-		Frame frame = new Frame(3,6);
-		Frame frame = new Frame(4,4);
-		Frame frame = new Frame(5,3);
-		Frame frame = new Frame(3,3);
-		Frame frame = new Frame(4,5);
-		Frame frame = new Frame(8,1);
-		Frame frame = new Frame(2,6);
+		Bgame.addFrame(frame);
+		
+		Frame frame2 = new Frame(3,6);
+		Bgame.addFrame(frame2);
+		
+		Frame frame3 = new Frame(7,2);
+		Bgame.addFrame(frame3);
+		
+		Frame frame4 = new Frame(3,6);
+		Bgame.addFrame(frame4);
+		
+		Frame frame5 = new Frame(4,4);
+		Bgame.addFrame(frame5);
+		
+		Frame frame6 = new Frame(5,3);
+		Bgame.addFrame(frame6);
+		
+		Frame frame7 = new Frame(3,3);
+		Bgame.addFrame(frame7);
+		
+		Frame frame8 = new Frame(4,5);
+		Bgame.addFrame(frame8);
+		
+		Frame frame9 = new Frame(8,1);
+		Bgame.addFrame(frame9);
+		
+		Frame frame10 = new Frame(2,6);
+		Bgame.addFrame(frame10);
 		
 		assertEquals([2,6], frame.());
 	}
